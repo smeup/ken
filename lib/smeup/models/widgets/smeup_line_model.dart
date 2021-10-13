215229f32dee553cd81bc27620280fc3b03e2109
Francesco Verzeletti <francesco.verzeletti@smeup.com>
2021-10-13 15:52:38 +0200

    refactoring line

--- lib/smeup/models/widgets/smeup_line_model.dart
+++ lib/smeup/models/widgets/smeup_line_model.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_components_library/smeup/daos/smeup_line_dao.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_model.dart';
+import 'package:mobile_components_library/smeup/services/smeup_data_service.dart';
 import 'package:mobile_components_library/smeup/services/smeup_utilities.dart';
 
 class SmeupLineModel extends SmeupModel {
-  Color color = Colors.white;
-  double thickness = 0;
+  static const Color defaultColor = Colors.black;
+  static const double defaultThickness = 5;
 
-  SmeupLineModel(GlobalKey<FormState> formKey, {this.color, this.thickness})
-      : super(formKey, title: '') {
+  Color color;
+  double thickness;
+
+  SmeupLineModel(
+    id,
+    type,
+    GlobalKey<FormState> formKey, {
+    this.color = defaultColor,
+    this.thickness = defaultThickness,
+  }) : super(formKey, title: '', id: id, type: type) {
     id = SmeupUtilities.getWidgetId('LIN', id);
   }
 
   SmeupLineModel.fromMap(
       Map<String, dynamic> jsonMap, GlobalKey<FormState> formKey)
       : super.fromMap(jsonMap, formKey) {
-    thickness = SmeupUtilities.getDouble(optionsDefault['thickness']) ?? 0;
+    thickness = SmeupUtilities.getDouble(optionsDefault['thickness']) ??
+        defaultThickness;
     if (optionsDefault['color'] != null) {
       color = SmeupUtilities.getColorFromRGB(optionsDefault['color']);
+    } else {
+      color = defaultColor;
     }
+    if (widgetLoadType != LoadType.Delay) {
+      SmeupLineDao.getData(this);
+    }
+
+    SmeupDataService.incrementDataFetch(id);
   }
 }
--- lib/smeup/widgets/smeup_line.dart
+++ lib/smeup/widgets/smeup_line.dart
@@ -1,22 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_components_library/smeup/daos/smeup_line_dao.dart';
+import 'package:mobile_components_library/smeup/models/smeupWidgetBuilderResponse.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_line_model.dart';
+import 'package:mobile_components_library/smeup/models/widgets/smeup_model.dart';
+import 'package:mobile_components_library/smeup/services/smeup_utilities.dart';
+import 'package:mobile_components_library/smeup/widgets/smeup_widget_interface.dart';
+import 'package:mobile_components_library/smeup/widgets/smeup_widget_mixin.dart';
+import 'package:mobile_components_library/smeup/widgets/smeup_widget_state_interface.dart';
+import 'package:mobile_components_library/smeup/widgets/smeup_widget_state_mixin.dart';
 
-class SmeupLine extends StatelessWidget {
-  final SmeupLineModel smeupLineModel;
-  final GlobalKey<ScaffoldState> scaffoldKey;
-  final GlobalKey<FormState> formKey;
+// ignore: must_be_immutable
+class SmeupLine extends StatefulWidget
+    with SmeupWidgetMixin
+    implements SmeupWidgetInterface {
+  SmeupLineModel model;
+  GlobalKey<ScaffoldState> scaffoldKey;
+  GlobalKey<FormState> formKey;
 
-  SmeupLine(
-    this.smeupLineModel,
+  Color color;
+  double thickness;
+  String title;
+  String id;
+  String type;
+
+  dynamic data;
+
+  SmeupLine(this.scaffoldKey, this.formKey,
+      {this.title,
+      this.id = '',
+      this.type = 'LIN',
+      this.color = SmeupLineModel.defaultColor,
+      this.thickness = SmeupLineModel.defaultThickness})
+      : super(key: Key(SmeupUtilities.getWidgetId(type, id))) {
+    id = SmeupUtilities.getWidgetId(type, id);
+  }
+
+  SmeupLine.withController(
+    this.model,
     this.scaffoldKey,
     this.formKey,
-  );
+  ) : super(key: Key(SmeupUtilities.getWidgetId(model.type, model.id))) {
+    runControllerActivities(model);
+  }
+
+  @override
+  runControllerActivities(SmeupModel model) {
+    SmeupLineModel m = model;
+    id = m.id;
+    type = m.type;
+    color = m.color;
+    thickness = m.thickness;
+    title = m.title;
+
+    data = treatData(m);
+  }
+
+  @override
+  dynamic treatData(SmeupModel model) {
+    SmeupLineModel m = model;
+
+    // change data format
+    return formatDataFields(m);
+  }
+
+  @override
+  _SmeupLineState createState() => _SmeupLineState();
+}
+
+class _SmeupLineState extends State<SmeupLine>
+    with SmeupWidgetStateMixin
+    implements SmeupWidgetStateInterface {
+  SmeupLineModel _model;
+  dynamic _data;
+
+  @override
+  void initState() {
+    _model = widget.model;
+    _data = widget.data;
+    if (_model != null) widgetLoadType = _model.widgetLoadType;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    runDispose(widget.scaffoldKey, widget.id);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Divider(
-      color: smeupLineModel.color,
-      thickness: smeupLineModel.thickness,
+    Widget line = runBuild(context, widget.id, widget.type, widget.scaffoldKey,
+        getInitialdataLoaded(_model), notifierFunction: () {
+      setState(() {
+        widgetLoadType = LoadType.Immediate;
+        setDataLoad(widget.id, false);
+      });
+    });
+
+    return line;
+  }
+
+  @override
+  Future<SmeupWidgetBuilderResponse> getChildren() async {
+    if (!getDataLoaded(widget.id) && widgetLoadType != LoadType.Delay) {
+      if (_model != null) {
+        await SmeupLineDao.getData(_model);
+        _data = widget.treatData(_model);
+      }
+      setDataLoad(widget.id, true);
+    }
+    final children = Divider(
+      color: widget.color,
+      thickness: widget.thickness,
     );
+    return SmeupWidgetBuilderResponse(_model, children);
   }
 }
