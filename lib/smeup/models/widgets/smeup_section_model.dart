89ddf027105eb7abec93a5f4b8cbc958dacd95ee
ac70 <antonio.cosentino@outlook.com>
2021-11-03 13:15:10 +0000

    Update smeup_section_model.dart

--- lib/smeup/models/widgets/smeup_section_model.dart
+++ lib/smeup/models/widgets/smeup_section_model.dart
@@ -9,7 +9,6 @@ import 'package:mobile_components_library/smeup/models/widgets/smeup_form_model.
 import 'package:mobile_components_library/smeup/models/widgets/smeup_gauge_model.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_image_list_model.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_image_model.dart';
-import 'package:mobile_components_library/smeup/models/widgets/smeup_input_field_model.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_inputpanel_model.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_label_model.dart';
 import 'package:mobile_components_library/smeup/models/widgets/smeup_line_model.dart';
