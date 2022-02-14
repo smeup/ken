7e95140615ae2e671bfc276b177bbb8ae53f28fd
ac70 <antonio.cosentino@outlook.com>
2022-02-14 13:11:57 +0000

    Update showcase_shared.dart

--- lib/smeup/screens/test/showcase_shared.dart
+++ lib/smeup/screens/test/showcase_shared.dart
@@ -1,7 +1,12 @@
 import 'package:ken/smeup/widgets/smeup_label.dart';
 
 class ShowCaseShared {
-  static getTestLabel(_scaffoldKey, _formKey, text, {height: 15}) {
-    return SmeupLabel(_scaffoldKey, _formKey, [text]);
+  static getTestLabel(_scaffoldKey, _formKey, text, {double height: 15}) {
+    return SmeupLabel(
+      _scaffoldKey,
+      _formKey,
+      [text],
+      height: height,
+    );
   }
 }
