4db60719a0a8f07f7f77bf6ac80e22e34078aa57
Dario Foresti <dario.foresti@smeup.com>
2022-02-15 12:37:20 +0100

    Drawer widget patches and showcase updates

--- lib/smeup/screens/test/drawer_screen.dart
+++ lib/smeup/screens/test/drawer_screen.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ken/smeup/models/widgets/smeup_drawer_data_element.dart';
 import 'package:ken/smeup/services/smeup_configuration_service.dart';
 import 'package:ken/smeup/widgets/smeup_drawer.dart';
-import 'package:ken/smeup/screens/test/calendar_screen.dart';
-import 'package:ken/smeup/screens/test/chart_screen.dart';
-import 'package:ken/smeup/screens/test/dashboard_screen.dart';
-import 'package:ken/smeup/screens/test/label_screen.dart';
+import 'label_screen.dart';
 
 class DrawerScreen extends StatelessWidget {
   static const routeName = '/DrawerScreen';
@@ -25,18 +22,18 @@ class DrawerScreen extends StatelessWidget {
               icon: Icon(Icons.arrow_back),
               onPressed: () => Navigator.pop(context, false),
             ),
-            title: Center(child: Text('Drawer Screen')),
+            title: Center(child: Text('Drawer')),
           ),
           endDrawer: _getDrawer(context),
           body: SingleChildScrollView(
             child: Container(
               padding: const EdgeInsets.all(20),
               child: Padding(
-                  padding: const EdgeInsets.only(top: 60.0),
+                  padding: const EdgeInsets.only(top: 30.0),
                   child: Center(
                     child: Text(
                       'Click on the drawer icon at the top right of the screen',
-                      style: TextStyle(fontSize: 25),
+                      //style: TextStyle(fontSize: 25),
                     ),
                   )),
             ),
@@ -49,15 +46,13 @@ class DrawerScreen extends StatelessWidget {
   SmeupDrawer _getDrawer(BuildContext context) {
     double groupFontSize = 18;
     double fontSize = 16;
-    int groupIcon = 58722;
+    int staticGroupIcon = 59110;
+    int dynamicGroupIcon = 57873;
 
     return SmeupDrawer(
       _scaffoldKey,
       _formKey,
       id: DrawerScreen.drawerId,
-      imageUrl: 'assets/images/sun.jpg',
-      imageHeight: 80,
-      imageWidth: 120,
       title: "my drawer",
       data: [
         SmeupDrawerDataElement(
@@ -70,36 +65,38 @@ class DrawerScreen extends StatelessWidget {
           'Label',
           route: LabelScreen.routeName,
           iconCode: 0,
-          group: 'GROUP 1',
+          group: 'STATIC',
           fontSize: fontSize,
-          groupIcon: groupIcon,
+          groupIcon: staticGroupIcon,
           groupFontSize: groupFontSize,
         ),
         SmeupDrawerDataElement(
           'Dashboard',
-          route: DashboardScreen.routeName,
+          route: 'F(EXD;*ROUTE;) 2(;;DashboardScreen)',
           iconCode: 0,
-          group: 'GROUP 1',
+          group: 'STATIC',
           fontSize: fontSize,
-          groupIcon: groupIcon,
+          groupIcon: staticGroupIcon,
           groupFontSize: groupFontSize,
         ),
         SmeupDrawerDataElement(
           'Chart',
-          route: ChartScreen.routeName,
+          route:
+              'F(EXD;*JSN;) 1(;;packages/ken/assets/jsons/forms) 2(;;test_chart)',
           iconCode: 0,
-          group: 'GROUP 2',
+          group: 'DYNAMIC',
           fontSize: fontSize,
-          groupIcon: groupIcon,
+          groupIcon: dynamicGroupIcon,
           groupFontSize: groupFontSize,
         ),
         SmeupDrawerDataElement(
           'Calendar',
-          route: CalendarScreen.routeName,
+          route:
+              'F(EXD;*JSN;) 1(;;packages/ken/assets/jsons/forms) 2(;;test_calendar)',
           iconCode: 0,
-          group: 'GROUP 2',
+          group: 'DYNAMIC',
           fontSize: fontSize,
-          groupIcon: groupIcon,
+          groupIcon: dynamicGroupIcon,
           groupFontSize: groupFontSize,
         )
       ],
