f512b005cdc9e249d562c178aaf6f88a88984d8e
Francesco Verzeletti <francesco.verzeletti@smeup.com>
2022-02-15 09:38:22 +0100

    fix padding gauge

--- lib/smeup/screens/test/gauge_screen.dart
+++ lib/smeup/screens/test/gauge_screen.dart
@@ -15,31 +15,31 @@ class GaugeScreen extends StatelessWidget {
       child: Builder(
         builder: (BuildContext context) => Scaffold(
           appBar: AppBar(
-            title: Center(child: Text('Gauge Screen')),
+            title: Center(child: Text('Gauge')),
             actions: ShowCaseShared.getEmptyAction(),
           ),
           body: SingleChildScrollView(
             child: Container(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: Center(
-                    child: Column(
-                  children: [
-                    ShowCaseShared.getTestLabel(_scaffoldKey, _formKey,
-                        'Highly customizable, feature-packed gauge widget for Flutter'),
-                    SmeupGauge(
-                      _scaffoldKey,
-                      _formKey,
-                      id: 'gau1',
-                      value: 120,
-                      maxValue: 150,
-                      minValue: 50,
-                      warning: 100,
-                    ),
-                  ],
-                )),
-              ),
+              padding: const EdgeInsets.all(30),
+              //child: Padding(
+              //padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                  child: Column(
+                children: [
+                  ShowCaseShared.getTestLabel(_scaffoldKey, _formKey,
+                      'Highly customizable, feature-packed gauge widget for Flutter'),
+                  SmeupGauge(
+                    _scaffoldKey,
+                    _formKey,
+                    id: 'gau1',
+                    value: 120,
+                    maxValue: 150,
+                    minValue: 50,
+                    warning: 100,
+                  ),
+                ],
+              )),
+              //),
             ),
           ),
         ),
