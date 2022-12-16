fdd64a8183058a4a5a41a387195e2b86a5ca7779
ac70 <antonio.cosentino@outlook.com>
2022-12-16 14:37:16 +0000

    restore test

--- test/widget_test/smeupLine_test.dart
+++ test/widget_test/smeupLine_test.dart
@@ -1,39 +1,46 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:ken/smeup/widgets/smeup_line.dart';
-// import 'package:ken/smeup/screens/test/line_screen.dart';
-// import 'widget_test_service.dart';
-//
-// Future<void> main() async {
-//   testWidgets('Test static contructor ', (WidgetTester tester) async {
-//     await WidgetTestService.initTests();
-//
-//     Widget testWidget = new MediaQuery(
-//         data: new MediaQueryData(), child: new MaterialApp(home: LineScreen()));
-//
-//     await tester.pumpWidget(testWidget).then((value) async {
-//       await tester.pumpAndSettle();
-//
-//       runTests();
-//     });
-//   });
-//
-//   testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
-//     await WidgetTestService.initTests();
-//
-//     final testWidget = await WidgetTestService.getDynamicScreen('test_line');
-//
-//     await tester.pumpWidget(testWidget).then((value) async {
-//       await tester.pumpAndSettle();
-//       runTests();
-//     });
-//   });
-// }
-//
-// runTests() {
-//   final findKey = find.byKey(Key('lin1'));
-//   expect(findKey, findsWidgets);
-//
-//   var findWidget = find.byType(SmeupLine);
-//   expect(findWidget, findsWidgets);
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ken/smeup/widgets/kenLine.dart';
+import 'widget_test_service.dart';
+
+Future<void> main() async {
+  testWidgets('Test static contructor ', (WidgetTester tester) async {
+    await WidgetTestService.initTests();
+
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+            home: KenLine(
+          _scaffoldKey,
+          _formKey,
+          id: 'lin1',
+        )));
+
+    await tester.pumpWidget(testWidget).then((value) async {
+      await tester.pumpAndSettle();
+      runTests();
+    });
+  });
+
+  // testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
+  //   await WidgetTestService.initTests();
+
+  //   final testWidget = await WidgetTestService.getDynamicScreen('test_line');
+
+  //   await tester.pumpWidget(testWidget).then((value) async {
+  //     await tester.pumpAndSettle();
+  //     runTests();
+  //   });
+  // });
+}
+
+runTests() {
+  final findKey = find.byKey(Key('lin1'));
+  expect(findKey, findsWidgets);
+
+  var findWidget = find.byType(KenLine);
+  expect(findWidget, findsWidgets);
+}
