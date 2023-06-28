203932ee63bca890a6be321a0adc2f0d1eb9dbf8
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2023-06-28 12:04:19 +0200

    restore chart file already commented

---
+++ lib/smeup/models/widgets/ken_chart_series.dart
@@ -0,0 +1,9 @@
+// import 'package:charts_flutter/flutter.dart';
+
+// class KenChartSeries {
+//   String code;
+//   double value;
+//   Color color;
+
+//   KenChartSeries(this.code, this.value, this.color);
+// }
