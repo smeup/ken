bd582903dc7f0684f7514e7c37940eb13a950646
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2023-06-30 10:11:00 +0200

    Restored and refactored datepicker and time picker

--- test/widget_test/kenDatePicker_test.dart
+++ test/widget_test/kenDatePicker_test.dart
@@ -1,75 +1,75 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:ken/smeup/widgets/kenDatepicker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ken/smeup/widgets/kenDatepicker.dart';
 
-// import 'widget_test_service.dart';
+import 'widget_test_service.dart';
 
-// final datePickerDynamicScreen = 'test_date_picker';
+final datePickerDynamicScreen = 'test_date_picker';
 
-// Future<void> main() async {
-//   testWidgets('Test static contructor ', (WidgetTester tester) async {
-//     await WidgetTestService.initTests();
+Future<void> main() async {
+  testWidgets('Test static contructor ', (WidgetTester tester) async {
+    await WidgetTestService.initTests();
 
-//     Widget testWidget = new MediaQuery(
-//         data: new MediaQueryData(),
-//         child: new MaterialApp(
-//             home: Scaffold(
-//           appBar: AppBar(),
-//           body: SingleChildScrollView(
-//             child: Container(
-//               //padding: const EdgeInsets.all(30),
-//               //child: Padding(
-//               padding: const EdgeInsets.all(30),
-//               child: Center(
-//                   child: Column(
-//                 children: [
-//                   KenDatePicker(
-//                     WidgetTestService.scaffoldKey,
-//                     WidgetTestService.formKey,
-//                     KenDatePickerData(value: DateTime(2021, 03, 21)),
-//                     id: "datepicker1",
-//                     underline: true,
-//                     label: "",
-//                     width: 400,
-//                     height: 600,
-//                   )
-//                 ],
-//               )),
-//               //),
-//             ),
-//           ),
-//         )));
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+            home: Scaffold(
+          appBar: AppBar(),
+          body: SingleChildScrollView(
+            child: Container(
+              //padding: const EdgeInsets.all(30),
+              //child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Center(
+                  child: Column(
+                children: [
+                  KenDatePicker(
+                    WidgetTestService.scaffoldKey,
+                    WidgetTestService.formKey,
+                    KenDatePickerData(value: DateTime(2021, 03, 21)),
+                    id: "datepicker1",
+                    underline: true,
+                    label: "",
+                    width: 400,
+                    height: 600,
+                  )
+                ],
+              )),
+              //),
+            ),
+          ),
+        )));
 
-//     await tester.pumpWidget(testWidget).then((value) async {
-//       await tester.pumpAndSettle();
+    await tester.pumpWidget(testWidget).then((value) async {
+      await tester.pumpAndSettle();
 
-//       runTests();
-//     });
-//   });
+      runTests();
+    });
+  });
 
-//   // testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
-//   //   await WidgetTestService.initTests();
+  // testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
+  //   await WidgetTestService.initTests();
 
-//   //   final testWidget =
-//   //       await WidgetTestService.getDynamicScreen(datePickerDynamicScreen);
+  //   final testWidget =
+  //       await WidgetTestService.getDynamicScreen(datePickerDynamicScreen);
 
-//   //   await tester.pumpWidget(testWidget).then((value) async {
-//   //     await tester.pumpAndSettle();
-//   //     runTests();
-//   //   });
-//   // });
-// }
+  //   await tester.pumpWidget(testWidget).then((value) async {
+  //     await tester.pumpAndSettle();
+  //     runTests();
+  //   });
+  // });
+}
 
-// runTests() {
-//   //final findKey = find.byKey(Key(DatePickerScreen.datePickerId));
-//   //expect(findKey, findsWidgets);
+runTests() {
+  //final findKey = find.byKey(Key(DatePickerScreen.datePickerId));
+  //expect(findKey, findsWidgets);
 
-//   var findWidget = find.byType(KenDatePicker);
-//   expect(findWidget, findsWidgets);
+  var findWidget = find.byType(KenDatePicker);
+  expect(findWidget, findsWidgets);
 
-//   var findText = find.byType(Text);
-//   expect(findText, findsWidgets);
+  var findText = find.byType(Text);
+  expect(findText, findsWidgets);
 
-//   var finderTextContent = find.text('21/03/2021');
-//   expect(finderTextContent, findsWidgets);
-// }
+  var finderTextContent = find.text('21/03/2021');
+  expect(finderTextContent, findsWidgets);
+}
--- test/widget_test/kenTimePicker_test.dart
+++ test/widget_test/kenTimePicker_test.dart
@@ -1,76 +1,76 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:ken/smeup/widgets/kenTimepicker.dart';
-// import 'widget_test_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ken/smeup/widgets/kenTimepicker.dart';
+import 'widget_test_service.dart';
 
-// final timePickerDynamicScreen = 'test_time_picker';
-// final timePickerData = new KenTimePickerData(
-//   time: DateTime(2021, 1, 1, 17, 30),
-//   formattedTime: "17:30",
-// );
+final timePickerDynamicScreen = 'test_time_picker';
+final timePickerData = new KenTimePickerData(
+  time: DateTime(2021, 1, 1, 17, 30),
+  formattedTime: "17:30",
+);
 
-// Future<void> main() async {
-//   testWidgets('Test static contructor ', (WidgetTester tester) async {
-//     await WidgetTestService.initTests();
+Future<void> main() async {
+  testWidgets('Test static contructor ', (WidgetTester tester) async {
+    await WidgetTestService.initTests();
 
-//     Widget testWidget = new MediaQuery(
-//         data: new MediaQueryData(),
-//         child: new MaterialApp(
-//             home: Scaffold(
-//           appBar: AppBar(),
-//           body: SingleChildScrollView(
-//             child: Container(
-//               padding: const EdgeInsets.all(30),
-//               //child: Padding(
-//               //padding: const EdgeInsets.only(top: 60.0),
-//               child: Center(
-//                   child: Column(
-//                 children: [
-//                   KenTimePicker(
-//                     WidgetTestService.scaffoldKey,
-//                     WidgetTestService.formKey,
-//                     KenTimePickerData(
-//                         time: DateTime(2021, 1, 1, 17, 30),
-//                         formattedTime: "17:30"),
-//                     id: 'timePicker',
-//                     width: 400,
-//                     label: "",
-//                     underline: true,
-//                   ),
-//                 ],
-//               )),
-//               //),
-//             ),
-//           ),
-//         )));
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+            home: Scaffold(
+          appBar: AppBar(),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              //child: Padding(
+              //padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                  child: Column(
+                children: [
+                  KenTimePicker(
+                    WidgetTestService.scaffoldKey,
+                    WidgetTestService.formKey,
+                    KenTimePickerData(
+                        time: DateTime(2021, 1, 1, 17, 30),
+                        formattedTime: "17:30"),
+                    id: 'timePicker',
+                    width: 400,
+                    label: "",
+                    underline: true,
+                  ),
+                ],
+              )),
+              //),
+            ),
+          ),
+        )));
 
-//     await tester.pumpWidget(testWidget).then((value) async {
-//       await tester.pumpAndSettle();
+    await tester.pumpWidget(testWidget).then((value) async {
+      await tester.pumpAndSettle();
 
-//       runTests();
-//     });
-//   });
+      runTests();
+    });
+  });
 
-//   // testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
-//   //   await WidgetTestService.initTests();
+  // testWidgets('Test dynamic contructor ', (WidgetTester tester) async {
+  //   await WidgetTestService.initTests();
 
-//   //   final testWidget =
-//   //       await WidgetTestService.getDynamicScreen(timePickerDynamicScreen);
+  //   final testWidget =
+  //       await WidgetTestService.getDynamicScreen(timePickerDynamicScreen);
 
-//   //   await tester.pumpWidget(testWidget).then((value) async {
-//   //     await tester.pumpAndSettle();
-//   //     runTests();
-//   //   });
-//   // });
-// }
+  //   await tester.pumpWidget(testWidget).then((value) async {
+  //     await tester.pumpAndSettle();
+  //     runTests();
+  //   });
+  // });
+}
 
-// runTests() {
-//   var findWidget = find.byType(KenTimePicker);
-//   expect(findWidget, findsWidgets);
+runTests() {
+  var findWidget = find.byType(KenTimePicker);
+  expect(findWidget, findsWidgets);
 
-//   var findText = find.byType(Text);
-//   expect(findText, findsWidgets);
+  var findText = find.byType(Text);
+  expect(findText, findsWidgets);
 
-//   var finderTextContent = find.text(timePickerData.formattedTime!);
-//   expect(finderTextContent, findsWidgets);
-// }
+  var finderTextContent = find.text(timePickerData.formattedTime!);
+  expect(finderTextContent, findsWidgets);
+}
