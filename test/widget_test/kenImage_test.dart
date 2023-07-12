901e7a9bb50ff85af5469b8f990f9d2f44ef32fa
ac70 <antonio.cosentino@outlook.com>
2023-07-12 09:15:42 +0100

    Update kenImage_test.dart

--- test/widget_test/kenImage_test.dart
+++ test/widget_test/kenImage_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../../lib/smeup/widgets/KenImage.dart';
+import '../../lib/smeup/widgets/kenImage.dart';
 import 'widget_test_service.dart';
 
 Future<void> main() async {
