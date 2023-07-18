d4f8856544c960e9d229931f1f57dd842d04d381
Alessandro De Roma <alessandro.deroma@we-plus.eu>
2023-07-18 11:59:14 +0200

    removed getDataQRCodeReader from data initializer interface

--- lib/smeup/services/ken_data_initializer_interface.dart
+++ lib/smeup/services/ken_data_initializer_interface.dart
@@ -4,7 +4,6 @@ import '../models/widgets/ken_screen_model.dart';
 
 abstract class KenDataInitializerInterface {
   bool isFirestore(KenModel model);
-  Future<void> getDataQRCodeReader(KenModel model);
   Future<void> getValidation(KenModel model);
   Future<void> setData(KenScreenModel model);
   Future<void> fromMap(Map<dynamic, dynamic> jsonMap, KenModel model);
