3a0032773ae17c205fe62c940def960fb9c024b4
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2023-09-07 10:30:21 +0200

    update

--- test/widget_test/kenSwitch_test.dart
+++ test/widget_test/kenSwitch_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ken/smeup/services/ken_log_service.dart';
-import '../../lib/smeup/widgets/kenSwitch.dart';
+import 'package:ken/smeup/widgets/kenSwitch.dart';
 
 import 'widget_test_service.dart';
 
