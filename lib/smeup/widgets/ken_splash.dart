4117e925fadf1f1b02a1fdcfa445130ddb2f3c0b
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2023-10-11 16:18:05 +0200

    Splash decouple

---
+++ lib/smeup/widgets/ken_splash.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../models/widgets/ken_splash_model.dart';
+import '../services/ken_defaults.dart';
+
+// ignore: must_be_immutable
+class KenSplash extends StatefulWidget {
+  GlobalKey<ScaffoldState> scaffoldKey;
+  GlobalKey<FormState>? formKey;
+
+  Color? color;
+  String? id;
+  String? type;
+  String? title;
+
+  KenSplash(this.scaffoldKey, this.formKey,
+      {this.color = KenSplashDefaults.defaultColor,
+      id = '',
+      this.title = KenSplashDefaults.title,
+      type = 'SPL'});
+
+  @override
+  State<KenSplash> createState() => KenSplashState();
+}
+
+class KenSplashState extends State<KenSplash> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget splash;
+
+    splash = Container(
+      color: widget.color,
+    );
+
+    return splash;
+  }
+}
